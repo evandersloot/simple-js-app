f3e29d20aeb76840f09bc25e953cb563ef1f5aa2
evandersloot <edvandersloot@gmail.com>
2021-06-25 16:23:22 -0500

    Update scripts.js with array and objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,34 @@
-alert('Hello World!');
+// add blank array
+var pokemonList = [];
 
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
+// add objects to array
+let pokemonList = [
+  {
+    name: 'Charmander',
+    height: '2.00',
+    weight: '18.7',
+    type: 'fire',
+    healthpoints: '70',
+  },
+  {
+    name: 'BeeDrill',
+    height: '3.03',
+    weight: '65',
+    type: 'bug',
+    healthpoints: '120',
+  },
+  {
+    name: 'Shuppet',
+    height: '2.00',
+    weight: '5.2',
+    type: 'ghost',
+    healthpoints: '60',
+  },
+  {
+    name: 'Galvantula',
+    height: '2.07',
+    weight: '31.5',
+    type: ['electric', 'bug'],
+    healthpoints: '90',
+  },
+];
