1b2b6b51cfea18153901950295a07f2cecceedcb
evandersloot <edvandersloot@gmail.com>
2021-07-07 12:35:26 -0500

    Update scripts.js with API fetching

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,11 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: 'Charmander',
-      height: 2.00,
-      weight: 18.7,
-      type: 'fire',
-      healthPoints: 70,
-    },
-    {
-      name: 'BeeDrill',
-      height: 3.03,
-      weight: 65,
-      type: 'bug',
-      healthPoints: 120,
-    },
-    {
-      name: 'Shuppet',
-      height: 2.00,
-      weight: 5.2,
-      type: 'ghost',
-      healthPoints: 60,
-    },
-    {
-      name: 'Galvantula',
-      height: 2.07,
-      weight: 31.5,
-      type: ['electric', 'bug'],
-      healthPoints: 90,
-    },
-  ];
+  let pokemonList = []
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
-      'name' in pokemon &&
-      'height' in pokemon &&
-      'weight' in pokemon &&
-      'type' in pokemon &&
-      'healthPoints' in pokemon
+      'name' in pokemon
     ) {
     pokemonList.push(pokemon);
     } else {
@@ -48,38 +16,70 @@ let pokemonRepository = (function () {
   function getAll() {
     return pokemonList;
     }
-// added elements and appended them
+
   function addListItem(pokemon){
-    let pokemonSelect = document.querySelector('.pokemon-list');
+    let pokemonList = document.querySelector('.pokemon-list');
     let listpokemon = document.createElement('li');
     let button = document.createElement('button');
     button.innerText = pokemon.name;
     button.classList.add('button-class');
     listpokemon.appendChild(button);
-    pokemonSelect.appendChild(listpokemon);
-// added event handler
+    pokemonList.appendChild(listpokemon);
+
     button.addEventListener('click', function(event){
       showDetails(pokemon);
     });
   }
-// added showDetails and log each click
-  function showDetails(pokemon){
-      console.log(pokemon.name);
+
+  function loadList(pokemon) {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
   }
 
+  function loadDetails(pokemon) {
+    let url = pokemon.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      pokemon.imageUrl = details.sprites.front_default;
+      pokemon.height = details.height;
+      pokemon.weight = details.weight;
+      pokemon.types = details.types;
+      pokemon.healthpoints = details.healthpoints;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(pokemon){
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon.name);
+    });
+  }
 
   return {
     getAll: getAll,
     add: add,
+    loadList: loadList,
+    loadDetails: loadDetails,
     addListItem: addListItem
     };
 })();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Ralts', height: 1.04, weight: 14.6, type: 'fairy', healthPoints: 60 });
-
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
